228e4106184639385c6d3c67048016c0536f73dd
wyvette0542 <yvette941208@gmail.com>
2017-10-25 23:43:39 +0000

    kernel

---
+++ hw07/gpio_test/.gpio_test.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/debian/ECE497/hw07/gpio_test/gpio_test.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -T ./arch/arm/kernel/module.lds -o /home/debian/ECE497/hw07/gpio_test/gpio_test.ko /home/debian/ECE497/hw07/gpio_test/gpio_test.o /home/debian/ECE497/hw07/gpio_test/gpio_test.mod.o
---
+++ hw07/gpio_test/.tmp_versions/gpio_test.mod
@@ -0,0 +1,2 @@
+/home/debian/ECE497/hw07/gpio_test/gpio_test.ko
+/home/debian/ECE497/hw07/gpio_test/gpio_test.o
