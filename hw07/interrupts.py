8d0866e1346e2e664c333658690862befc382afe
wyvette0542 <yvette941208@gmail.com>
2017-10-24 22:16:48 -0400

    interrupt python

---
+++ hw07/interrupts.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+# setup input and output GPIO
+button = "GP1_3"
+LED = "GP1_4"
+GPIO.setup(button, GPIO.IN)
+GPIO.setup(LED, GPIO.OUT)
+
+# map button to led
+map = {button: LED}
+
+def updateLED(channel):
+	print("channel = " + channel)
+	state = GPIO.input(channel)
+	GPIO.output(mat[channel], state)
+	print(map[channel] + " Toggled")
+
+print("Running...")
+
+GPIO.add_event_detect(button, GPIO.BOTH, callback=updateLED)
+
+try:
+	while True:
+		time.sleep(100)
+
+except KeyboardInterrupt:
+	print("Cleaning Up")
+	GPIO.cleanup()
+GPIO.cleanup()
+
